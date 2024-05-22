4b13debf942cefcd01e7b071eb59b92a9d60a111
NahumFG <ngutierrez2507@gmail.com>
2024-05-21 19:36:08 -0500

    backend: configuración de temporales de media y static

--- backend/api_ecommerce_project/settings.py
+++ backend/api_ecommerce_project/settings.py
@@ -127,11 +127,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [BASE_DIR / "temp" / "static"]
+STATIC_ROOT = BASE_DIR / "temp" / "staticfiles"
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = BASE_DIR / "temp" / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
