b0428eb9fa8b2310d2ed889f4635207e308ac2b7
NahumFG <ngutierrez2507@gmail.com>
2024-05-21 22:11:50 -0500

    backend: modificación de signals

--- backend/user_auth/signals.py
+++ backend/user_auth/signals.py
@@ -7,12 +7,21 @@ User = get_user_model()
 
 
 @receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created and not hasattr(instance, "profile"):
-        Profile.objects.create(user=instance)
-
-
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    if hasattr(instance, "profile"):
-        instance.profile.save()
+def manage_user_profile(sender, instance, created, **kwargs):
+    if created:
+        # Si el usuario es nuevo, se crea un perfil
+        Profile.objects.create(
+            user=instance,
+            first_name=instance.first_name,
+            last_name=instance.last_name,
+            gender=instance.gender,
+            birth_date=instance.birth_date,
+        )
+    else:
+        # Si el usuario ya existe, se actualiza el perfil
+        if hasattr(instance, "profile"):
+            instance.profile.first_name = instance.first_name
+            instance.profile.last_name = instance.last_name
+            instance.profile.gender = instance.gender
+            instance.profile.birth_date = instance.birth_date
+            instance.profile.save()
