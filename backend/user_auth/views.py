ad2d11476aa99763f5aabec51b98d25cbd86ee8a
NahumFG <ngutierrez2507@gmail.com>
2024-05-23 10:48:35 -0500

    backend: Delete de users, solo el superuser

--- backend/user_auth/views.py
+++ backend/user_auth/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics, status
-from rest_framework.exceptions import ValidationError
+from rest_framework.exceptions import PermissionDenied, ValidationError
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -21,6 +21,13 @@ class UserApiViewSet(ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
     permission_classes = [IsAuthenticated]
+    http_method_names = ["get", "post", "patch", "delete"]
+
+    def destroy(self, request, *args, **kwargs):
+        user = request.user
+        if not user.is_superuser:
+            raise PermissionDenied("You do not have permission to delete this user.")
+        return super().destroy(request, *args, **kwargs)
 
 
 class UserRegisterView(generics.CreateAPIView):
