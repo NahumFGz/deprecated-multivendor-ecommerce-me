d4a613c54bf41dadad62b58df738fba74bf315e7
NahumFG <ngutierrez2507@gmail.com>
2024-05-24 11:43:26 -0500

    frontend: Sign In login page

--- frontend/src/layouts/AuthLayout.jsx
+++ frontend/src/layouts/AuthLayout.jsx
@@ -1,9 +1,7 @@
 export function AuthLayout ({ children }) {
   return (
-    <div className='flex flex-col items-center justify-center h-screen bg-gray-200'>
-      <div className='flex flex-col items-center justify-center bg-white p-4 rounded-lg shadow-lg'>
-        {children}
-      </div>
+    <div className='h-screen'>
+      {children}
     </div>
   )
 }
