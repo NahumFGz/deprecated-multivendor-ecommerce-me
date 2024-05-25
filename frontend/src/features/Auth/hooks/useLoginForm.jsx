72d1c924966ba6b5d0333ee25d59d4b77f65321a
NahumFG <ngutierrez2507@gmail.com>
2024-05-24 23:33:16 -0500

    frontend: useLoginForm error al entrar varias veces

--- frontend/src/features/Auth/hooks/useLoginForm.jsx
+++ frontend/src/features/Auth/hooks/useLoginForm.jsx
@@ -3,10 +3,12 @@ import { useFormik } from 'formik'
 import { useAuthStore } from '../../../store/useAuthStore'
 import { toast } from 'react-toastify'
 import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export const useLoginForm = () => {
   const { login, error } = useAuthStore()
   const [loadingLogin, setloadingLogin] = useState(false)
+  const navigate = useNavigate()
 
   const validationSchema = Yup.object({
     email: Yup.string().email('Invalid email address').required('Required'),
@@ -31,11 +33,11 @@ export const useLoginForm = () => {
       if (error) {
         toast.error(error)
       } else {
-        toast.success('Login successful')
+        navigate('/home')
       }
       setloadingLogin(false)
     }
   }, [loadingLogin])
 
-  return { formik }
+  return { formik, loadingLogin }
 }
