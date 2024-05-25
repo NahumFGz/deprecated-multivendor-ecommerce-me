6c8f79b64b32055555e403f7e583f1e292d373ae
NahumFG <ngutierrez2507@gmail.com>
2024-05-25 00:57:38 -0500

    frontend: logout

--- frontend/src/layouts/HomeLayout.jsx
+++ frontend/src/layouts/HomeLayout.jsx
@@ -1,11 +1,15 @@
 import { Link } from 'react-router-dom'
+import { useAuthStore } from '../store/useAuthStore'
 
 export function HomeLayout ({ children }) {
+  const logout = useAuthStore((store) => store.logout)
+
   return (
     <>
       <div className='flex flex-row gap-14'>
         <h1>Home Layout</h1>
-        <Link to='/login'> <h1 className='text-red-400'>Login</h1> </Link>
+        <Link to='/login'> <h1 className='text-green-400'>Login</h1> </Link>
+        <h1 onClick={logout} className='text-red-400 cursor-pointer'>Logout</h1>
       </div>
       <hr className='border' />
       <div>
