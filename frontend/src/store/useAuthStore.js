69b767cffddb17ef6935f936a9c9ca5b42681452
NahumFG <ngutierrez2507@gmail.com>
2024-05-25 17:06:35 -0500

    frontend: refactorizando store

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,39 +1,6 @@
 import { create } from 'zustand'
-import { setItem as setLocalItem, getItem as getLocalItem, removeItem as removeLocalItem } from '../services/storage/localStorage'
-import { loginAccessApi } from '../services/api/authService'
 
 export const useAuthStore = create((set) => ({
-  token: getLocalItem('token') || null,
-  refreshToken: getLocalItem('refreshToken') || null,
-  isLoading: false,
-  error: null,
-
-  login: async (email, password) => {
-    set({ isLoading: true, error: null })
-    try {
-      const data = await loginAccessApi(email, password)
-      setLocalItem('token', data.access)
-      setLocalItem('refreshToken', data.refresh)
-
-      set({ token: data.access, refreshToken: data.refresh, isLoading: false })
-    } catch (error) {
-      set({ error: error.message, isLoading: false })
-      throw new Error(error.message)
-    }
-  },
-
-  logout: () => {
-    set({ isLoading: true, error: null })
-    try {
-      removeLocalItem('token')
-      removeLocalItem('refreshToken')
-
-      set({ token: null, refreshToken: null, isLoading: false })
-    } catch (error) {
-      set({ error: error.message, isLoading: false })
-      throw new Error(error.message)
-    }
-  },
-
-  clearError: () => set({ error: null })
+  token: null,
+  setToken: (token) => set({ token })
 }))
