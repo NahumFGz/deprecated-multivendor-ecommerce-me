7db9b322747e7430ae92ccfa4db153555470807b
NahumFG <ngutierrez2507@gmail.com>
2024-05-26 17:14:32 -0500

    frontend: forgot password form

--- frontend/src/features/Auth/components/ForgotPasswordForm.jsx
+++ frontend/src/features/Auth/components/ForgotPasswordForm.jsx
@@ -33,7 +33,9 @@ export function ForgotPasswordForm () {
             type='email'
             name='email'
             id='email'
-            className='block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+            className={`block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6 ${
+              formik.touched.email && formik.errors.email ? 'ring-red-500 focus:ring-red-500' : 'ring-gray-300 focus:ring-indigo-600'
+            }`}
             placeholder='your-mail@example.com'
             value={formik.values.email}
             onChange={formik.handleChange}
