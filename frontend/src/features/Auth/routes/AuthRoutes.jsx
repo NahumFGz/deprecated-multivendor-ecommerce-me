0afa63e9a8b5aeb6eb5716602c5e9402cf4813b7
NahumFG <ngutierrez2507@gmail.com>
2024-05-26 13:57:18 -0500

    frontend: corrección de layouts

--- frontend/src/features/Auth/routes/AuthRoutes.jsx
+++ frontend/src/features/Auth/routes/AuthRoutes.jsx
@@ -2,11 +2,12 @@ import { Routes, Route } from 'react-router-dom'
 import { LoginPage } from '../pages/LoginPage'
 import { RegisterPage } from '../pages/RegisterPage'
 import { ForgotPasswordPage } from '../pages/ForgotPasswordPage'
+import { AuthLayout } from '../../../layouts/AuthLayout'
 
 export function AuthRoutes () {
   return (
     <Routes>
-      <Route path='/login' element={<LoginPage />} />
+      <Route path='/login' element={<AuthLayout><LoginPage /> </AuthLayout>} />
       <Route path='/register' element={<RegisterPage />} />
       <Route path='/forgot-password' element={<ForgotPasswordPage />} />
     </Routes>
--- frontend/src/layouts/AuthLayout.jsx
+++ frontend/src/layouts/AuthLayout.jsx
@@ -1,6 +1,7 @@
 export function AuthLayout ({ children }) {
   return (
     <div className='h-screen'>
+      <p>Auth Layout</p>
       {children}
     </div>
   )
