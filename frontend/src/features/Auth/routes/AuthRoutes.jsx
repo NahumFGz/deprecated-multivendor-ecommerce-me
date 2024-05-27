2443d6073a75ce1c12ef3248fc2fcdaa115cff4c
NahumFG <ngutierrez2507@gmail.com>
2024-05-26 19:02:26 -0500

    frontend: Auth navigate

--- frontend/src/features/Auth/routes/AuthRoutes.jsx
+++ frontend/src/features/Auth/routes/AuthRoutes.jsx
@@ -1,13 +1,14 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import { LoginPage } from '../pages/LoginPage'
 import { RegisterPage } from '../pages/RegisterPage'
 import { AuthLayout } from '../../../layouts/AuthLayout'
 import { PasswordResetPage } from '../pages/PasswordResetPage'
-import { authUrls as urls } from './authUrls'
+import { authUrls as urls, authBasePath as baseUrl } from './authUrls'
 
 export function AuthRoutes () {
   return (
     <Routes>
+      <Route path={baseUrl} element={<Navigate to={urls.login} />} />
       <Route path={urls.login} element={<AuthLayout><LoginPage /></AuthLayout>} />
       <Route path={urls.register} element={<AuthLayout><RegisterPage /></AuthLayout>} />
       <Route path={urls.passwordReset} element={<AuthLayout><PasswordResetPage /></AuthLayout>} />
