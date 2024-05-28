26a2370a74545e1427a7ad05681209d2ad2f8469
NahumFG <ngutierrez2507@gmail.com>
2024-05-28 10:12:36 -0500

    frontend: Limpiar HomeLayout.jsx

--- frontend/src/features/Home/layouts/HomeLayout.jsx
+++ frontend/src/features/Home/layouts/HomeLayout.jsx
@@ -1,25 +1,7 @@
-import { Link } from 'react-router-dom'
-import { useAuthStore } from '../../../store/useAuthStore'
-import { useAuthAPI } from '../../Auth/hooks/useAuthAPI'
 import { Header } from '../components/Header'
 import { HeaderSearch } from '../components/HeaderSearch'
 
 export function HomeLayout ({ children }) {
-  const cleanStore = useAuthStore((store) => store.cleanStore)
-  const isAuth = useAuthStore((store) => store.isAuth)
-  const { authMe } = useAuthAPI()
-
-  const handleLogout = () => {
-    console.log('Logging out...')
-    cleanStore()
-  }
-
-  const handleGetProfile = async () => {
-    console.log('Getting profile...')
-    const response = await authMe()
-    console.log('Profile:', response)
-  }
-
   return (
     <>
       <HeaderSearch />
@@ -27,19 +9,6 @@ export function HomeLayout ({ children }) {
       <div>
         {children}
       </div>
-
-      <hr className='border mt-10' />
-      <div className='flex flex-row gap-14'>
-        <h1>Home Layout</h1>
-        {
-          isAuth
-            ? <button onClick={handleGetProfile}> Get Profile</button>
-            : <Link to='/auth/login'> <h1 className='text-green-400'>Login</h1> </Link>
-        }
-        <h1 onClick={handleLogout} className='text-red-400 cursor-pointer'>Logout</h1>
-      </div>
-      <hr className='border' />
-
     </>
   )
 }
