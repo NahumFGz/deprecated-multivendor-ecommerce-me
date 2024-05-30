2da4870bc4f6a96025ad1de217d1faf6ea15f2d1
NahumFG <ngutierrez2507@gmail.com>
2024-05-30 16:43:59 -0500

    backend: definición de modelos abstractos

---
+++ backend/core/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+
+# Create your models here.
+class TimeStampModel(models.Model):
+    created_at = models.DateTimeField(db_index=True, auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class UUIDModel(models.Model):
+    """
+    Best practice for lookup field url instead pk or slug for security
+    """
+
+    uuid = models.UUIDField(db_index=True, unique=True, editable=False)
+
+    class Meta:
+        abstract = True
+
+
+class TimeStampUUIDModel(models.Model):
+    """
+    Best practice for lookup field url instead pk or slug for security
+    """
+
+    uuid = models.UUIDField(db_index=True, unique=True, editable=False)
+    created_at = models.DateTimeField(db_index=True, auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
