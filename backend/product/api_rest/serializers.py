206100a44a44deb0ca5fd4acc1c3d919553ce01f
NahumFG <ngutierrez2507@gmail.com>
2024-05-31 13:01:14 -0500

    backend: get kinds

--- backend/product/api_rest/serializers.py
+++ backend/product/api_rest/serializers.py
@@ -10,15 +10,23 @@ class SubKindProductSerializer(serializers.ModelSerializer):
 
 
 class KindProductSerializer(serializers.ModelSerializer):
+    SubKindProduct = SubKindProductSerializer(many=True, read_only=True)
+
     class Meta:
         model = KindProduct
         fields = "__all__"
 
 
 class CategorySerializer(serializers.ModelSerializer):
+    kinds = serializers.SerializerMethodField()
+
     class Meta:
         model = Category
         fields = "__all__"
+
+    def get_kinds(self, obj):
+        kinds = KindProduct.objects.filter(category=obj).order_by("-modified_at")
+        return KindProductSerializer(kinds, many=True).data
 
 
 class ProductSerializer(serializers.ModelSerializer):
