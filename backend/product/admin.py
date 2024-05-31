b548c728666db09a4d200e1263b46559dcbfbd8f
NahumFG <ngutierrez2507@gmail.com>
2024-05-31 11:56:41 -0500

    backend: register product

--- backend/product/admin.py
+++ backend/product/admin.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
 
-from product.models import Category, KindProduct, SubKindProduct
+from product.models import Category, KindProduct, Product, SubKindProduct
 
 
 # Register your models here.
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    pass
+    readonly_fields = [
+        "image_small_size",
+        "image_medium_size",
+        "image_large_size",
+        "created_at",
+        "updated_at",
+    ]
 
 
 @admin.register(KindProduct)
@@ -16,4 +22,9 @@ class KindProductAdmin(admin.ModelAdmin):
 
 @admin.register(SubKindProduct)
 class SubKindProductAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
     pass
