ca2ac551e16c49ae5c2875fefd07a07a669cf018
NahumFG <ngutierrez2507@gmail.com>
2024-05-31 11:20:41 -0500

    backend: install tinymce

--- backend/product/models.py
+++ backend/product/models.py
@@ -3,6 +3,7 @@ from django.db.models import signals
 from django.dispatch import receiver
 from django.utils.text import slugify
 from thumbnails.fields import ImageField
+from tinymce import models as tinymce_models
 
 from core.models import TimeStampModel, TimeStampUUIDModel
 
@@ -80,3 +81,50 @@ class SubKindProduct(TimeStampModel):
 @receiver(signals.pre_save, sender=SubKindProduct)
 def pre_save_sub_kind_product(sender, instance, **kwargs):
     instance.slug = slugify(instance.name)
+
+
+class Product(TimeStampUUIDModel):
+    GENDER_CHOICES = [
+        ("M", "Male"),
+        ("F", "Female"),
+        ("O", "Other"),
+    ]
+
+    category = models.ForeignKey(
+        Category, related_name="product_category", blank=True, null=True, on_delete=models.CASCADE
+    )
+    kind_of_product = models.ForeignKey(
+        KindProduct, related_name="product_kind", blank=True, null=True, on_delete=models.CASCADE
+    )
+    sub_kind_of_product = models.ForeignKey(
+        SubKindProduct,
+        related_name="product_subkind",
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE,
+    )
+    title = models.CharField(max_length=400)
+    slug = models.SlugField(max_length=400, blank=True, null=True)
+    short_description = tinymce_models.HTMLField(default="", blank=True, null=True)
+    description = tinymce_models.HTMLField(default="", blank=True, null=True)
+    views = models.IntegerField(default=0)
+    rating = models.FloatField(default=0)
+    image_principal = ImageField(upload_to="product", blank=True, null=True)
+
+    def __str__(self):
+        return str(self.title)
+
+    @property
+    def image_small_size(self):
+        small_url = self.image_principal.thumbnail.small.url
+        return small_url
+
+    @property
+    def image_medium_size(self):
+        medium_url = self.image_principal.thumbnail.medium.url
+        return medium_url
+
+    @property
+    def image_large_size(self):
+        large_url = self.image_principal.thumbnail.large.url
+        return large_url
